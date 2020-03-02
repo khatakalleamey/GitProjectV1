6f1ce049db02b5efd054fa24783a1ac8bc92d952
Amey <khatakalleamey@gmail.com>
2020-03-02 18:20:18 +0530

    First Commit....

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String hungry = "Yes";
+		
+		if(hungry == "Yes") {
+			System.out.println(" Go ----");
+		}else {
+			System.out.println(" Dont Go --------");
+		}
+	}
+
+}
