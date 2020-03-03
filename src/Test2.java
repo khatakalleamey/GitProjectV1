a075add1b1cb75ef01882903acac75433f25de69
Amey <khatakalleamey@gmail.com>
2020-03-03 11:30:59 +0530

    Commit = Made New Class with Boolean Expression

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		Boolean isHungry = true;
+		
+		if(isHungry=true) {
+			System.out.println("Yes go....");
+		}else{
+		System.out.println(" --No-- ");	
+		}
+
+	}
+
+}
